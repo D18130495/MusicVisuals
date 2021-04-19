3eaaabad61a505b4138537163ca4791c2e6d2e6a
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-19 12:23:16 +0100

    Mouse Press function

--- java/src/D18130495/Control.java
+++ java/src/D18130495/Control.java
@@ -1,162 +1,62 @@
 package D18130495;
 
-import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
-import ddf.minim.AudioPlayer;
-import ddf.minim.Minim;
 import processing.core.PApplet;
 
 public class Control extends PApplet {
 
-    Minim minim;
-    AudioInput ai; //Connect to mic
-    AudioBuffer ab; //Samples
-    AudioPlayer ap;
+    int controlNum = 0;
+    
+    public int getControlNum() {
+        return controlNum;
+    }
 
-    float[] lerpedBuffer;
+    public void setControlNum(int controlNum) {
+        this.controlNum = controlNum;
+    }
 
     public void settings() {
-        size(512, 512);
-        //fullScreen(P3D, SPAN);
+        size(128, 512);
     }
-
-    float y = 200;
-    float lerpedY = y;
-    int which = 0;
 
     public void setup() {
-        minim = new Minim(this);
-        ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        ap = minim.loadFile("heroplanet.mp3", width);
-        ap.play();
-        ab = ai.mix; //buffer to the mic
-        ab = ap.mix; //buffer to the file
-        colorMode(HSB);
-        lerpedBuffer = new float[width];
+       colorMode(RGB);
     }
 
-    public void keyPressed() {
-        if(keyCode >= '0' && keyCode <= '5') {
-            which = keyCode - '0';
-        }
-        if(keyCode == ' ') {
-            if(ap.isPlaying()) {
-                ap.pause();
-            } else {
-                //ap.rewind();
-                ap.play();
-            }
+    void drawGrid()
+    {
+        for(int x = 1 ; x <= 4; x++)
+        {
+            float lh = map(x, 0, 5, 0, height);
+            line(0, lh, width, lh); 
         }
     }
 
-    float lerpedAverage = 0;
+    public void mousePressed()
+	{
+        if(mouseY < map(1, 0, 5, 0, height)) {
+            controlNum = 1;
+            println("Mouse 1");
+        }
+		else if(mouseY < map(2, 0, 5, 0, height)) {
+            controlNum = 2;
+            println("Mouse 2");
+        }
+        else if(mouseY < map(3, 0, 5, 0, height)) {
+            controlNum = 3;
+            println("Mouse 3");
+        }
+        else if(mouseY < map(4, 0, 5, 0, height)) {
+            controlNum = 4;
+            println("Mouse 4");
+        }
+        else if(mouseY < map(5, 0, 5, 0, height)) {
+            controlNum = 5;
+            println("Mouse 5");
+        }	
+	}
 
     public void draw() {
-        background(0);
-        stroke(255);
-        float halfHeight = height / 2;
-        float average = 0;
-        float sum = 0;
-        float lastX = width / 2, lastY = height / 2;
-
-        switch (which)
-        {
-            case 0:
-            {
-                for (int i = 0; i < ab.size(); i++) {
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    //println(lerpedBuffer[i]);
-                    lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-                    line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4, halfHeight + lerpedBuffer[i] * halfHeight * 4, i);
-                }
-                break;
-            }
-            case 1:
-            {
-                for (int i = 0; i < ab.size(); i++) {
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    //println(lerpedBuffer[i]);
-                    lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-                    line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4, i, halfHeight + lerpedBuffer[i] * halfHeight * 4);
-                }
-                break;
-            }
-            case 2:
-            {
-                for (int i = 0; i < ab.size(); i++) {
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    //println(lerpedBuffer[i]);
-                    lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-                    line(0, i, lerpedBuffer[i] * halfHeight * 8, i);
-                    line(i, 0, i, lerpedBuffer[i] * halfHeight * 8);
-                    line(width, i, width - (lerpedBuffer[i] * halfHeight * 8), i);
-                    line(i, height, i, height - (lerpedBuffer[i] * halfHeight * 8));
-                }
-                break;
-            }
-            case 3:
-            {
-                for (int i = 0; i < ab.size(); i++) {
-                    sum = sum + abs(ab.get(i));
-                    average = sum / (float)ab.size();
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    noFill();
-                    //println(lerpedBuffer[i]);
-                    lerpedBuffer[i] = lerp(lerpedBuffer[i], average, 0.1f);
-                    ellipse(width / 2, height / 2, lerpedBuffer[i] * 2000, lerpedBuffer[i] * 2000);
-                }
-                break;
-            }
-            case 4:
-            {
-                for (int i = 0; i < ab.size(); i++) {
-                    sum = sum + abs(ab.get(i));
-                    average = sum / (float)ab.size();
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    noFill();
-                    rectMode(CENTER);
-                    //println(lerpedBuffer[i]);
-                    lerpedBuffer[i] = lerp(lerpedBuffer[i], average, 0.1f);
-                    rect(width / 2, height / 2, lerpedBuffer[i] * 2000, lerpedBuffer[i] * 2000);
-                }
-                break;
-            }
-            case 5:
-            {
-                float r = 1f;
-                int numPoints = 10;
-                float thetaInc = TWO_PI / (float)numPoints;
-                strokeWeight(2);
-                for(int i = 0; i < 512; i++)
-                {
-                    float c = map(i, 0, 300, 0, 255) % 150.0f;
-                    stroke(c, 255, 255, 100);
-                    lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-                    float theta = i * (thetaInc + lerpedBuffer[i]);
-                    float x = width / 2 + sin(theta) * r;
-                    float y = height / 2 - cos(theta) * r;
-                    r += 0.5f + lerpedBuffer[i];
-                    line(lastX, lastY, x, y);
-                    lastX = x;
-                    lastY = y;
-                }
-                break;
-            }
-        }
-
-//            average = sum / (float)ab.size();
-//            lerpedAverage = lerp(lerpedAverage, average, 0.1f);
-//            noStroke();
-//            ellipse(width / 2, 100, lerpedAverage * 500, lerpedAverage * 500);
-
-//            ellipse(200, y, 30, 30);
-//            ellipse(300, lerpedY, 30, 30);
-//            y += random(-10, 10);
-//            lerpedY = lerp(lerpedY, y, 0.1f);
+        background(255, 100, 0);
+        drawGrid();
     }
 }
