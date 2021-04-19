75d5b08208d74d19cc6d427b431ac836c4421ae4
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-19 10:12:14 +0100

    add controler

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,8 +1,7 @@
 package ie.tudublin;
 
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
+import D18130495.YushunVisual;
+import D18130495.Control;
 
 public class Main
 {	
@@ -10,12 +9,19 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
+        processing.core.PApplet.runSketch( a, new YushunVisual());		
+	}
+
+	public void control()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Control());		
 	}
 
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.startUI();
+		main.control();			
 	}
 }
