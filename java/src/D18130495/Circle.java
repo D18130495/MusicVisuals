504656772b8f95be00d05e92ef2914b80e430631
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-25 15:09:52 +0100

    circle class use for third visual

---
+++ java/src/D18130495/Circle.java
@@ -0,0 +1,65 @@
+package D18130495;
+
+public class Circle {
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public void update1() {
+        int min = -30;
+        int max = 30;
+        x = x + min + (int)(Math.random() * (max - min +1));
+        y = y + 10;
+    }
+
+    public void update2() {
+        int min = -30;
+        int max = 30;
+        x = x + min + (int)(Math.random() * (max - min +1));
+        y = y - 10;
+    }
+
+    public void update3() {
+        int min = -30;
+        int max = 30;
+        x = x + 10;
+        y = y + min + (int)(Math.random() * (max - min +1));
+    }
+
+    public void update4() {
+        int min = -30;
+        int max = 30;
+        x = x - 10;
+        y = y + min + (int)(Math.random() * (max - min +1));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
