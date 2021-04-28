c39f3e67970ff4ed2ec6bf2b7056700bc644de04
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-04-28 19:30:43 +0100

    update some comments

--- java/src/D18130495/Draw.java
+++ java/src/D18130495/Draw.java
@@ -13,17 +13,21 @@ public class Draw
 
     public void render()
     {
-        yv.getFFT().forward(yv.getAudioPlayer().mix);
-        yv.setBands(yv.getFFT().getSpectrumReal());                
+        yv.getFFT().forward(yv.getAudioPlayer().mix); //load audio to fft
+        yv.setBands(yv.getFFT().getSpectrumReal()); //load fft to band         
         yv.noStroke();
        
         for(int j = 0; j < yv.rect.size(); j++) {
             yv.fill(255);
-            yv.rect(yv.rect.get(j).getX(), yv.rect.get(j).getY(), 3, 3);
+            yv.rect(yv.rect.get(j).getX(), yv.rect.get(j).getY(), 3, 3); //draw the rectangle(pixel)
 
-            if(j == (yv.rect.size() - 1))
+            if(j == (yv.rect.size() - 1)) //after draw all the rectangle from the buffer add new rectangle to the buffer
             {
-                
+                //Because the screen of my visual is 1024 X 640, so there are 655360 pixels on the screen.
+                //If I want make the image more clear, I have to draw as many pixels as possible(up tp 655360)
+                //The below if else statement use to speed up the drawing.
+                //When j less than 1024 it will more clearly see how the process of drawing is synchronized to the music.
+
                 if(j < 256 && yv.abs(yv.getBands()[1]) * 50 > 500) {
                     newRect();
                     yv.fill(255);
@@ -68,11 +72,11 @@ public class Draw
     }
 
     public void newRect() {
-        int p = (int)yv.random(0, yv.pixel.size());
+        int p = (int)yv.random(0, yv.pixel.size()); //pick a random pixel from pixel arraylist
         PVector r = yv.pixel.get(p);
         int x = (int)r.x;
         int y = (int)r.y;
-        yv.rect.add(new Rect(x, y));
-        yv.pixel.remove(p);
+        yv.rect.add(new Rect(x, y)); //add this pixel to the rect arraylist as a new  rectangle
+        yv.pixel.remove(p); //remove this pixel from pixel arraylist for not pick it again
     }    
 }
